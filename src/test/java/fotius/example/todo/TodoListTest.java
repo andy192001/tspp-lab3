cee67691132da7f0f5f523c3b962a169946ad4f7
Andy <andy192001d@gmail.com>
2023-08-13 21:05:26 +0300

    Add unit tests to TodoList

--- src/test/java/fotius/example/todo/TodoListTest.java
+++ src/test/java/fotius/example/todo/TodoListTest.java
@@ -3,6 +3,8 @@ package fotius.example.todo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class TodoListTest {
 
     // EXAMPLE TEST
@@ -12,5 +14,50 @@ class TodoListTest {
         TodoList list = new TodoList();
 
         Assertions.assertTrue(list.items().isEmpty());
+    }
+
+    @Test
+    void addElementToTodoList(){
+        TodoList list = new TodoList();
+
+        list.add("First task");
+        list.add("Second task");
+
+        Assertions.assertEquals("First task", list.items().get(0));
+        Assertions.assertEquals("Second task", list.items().get(1));
+    }
+
+    @Test
+    void getItemsOfTodoList(){
+        TodoList todoList = new TodoList(List.of("First task", "Second task"));
+
+        final List<String> items = todoList.items();
+
+        Assertions.assertEquals(List.of("First task", "Second task"), items);
+    }
+
+    @Test
+    void deleteElementFromTodoList(){
+        TodoList todoList = new TodoList(List.of("First task", "Second task"));
+
+        boolean isDeleted = todoList.delete("First task");
+        Assertions.assertTrue(isDeleted);
+    }
+
+    @Test
+    void deleteInvalidElementFromTodoList(){
+        TodoList todoList = new TodoList(List.of("First task", "Second task"));
+
+        boolean isDeleted = todoList.delete("Third task");
+        Assertions.assertFalse(isDeleted);
+    }
+
+    @Test
+    void deleteElementAtIndex(){
+        TodoList todoList = new TodoList(List.of("First task", "Second task"));
+
+        todoList.deleteAt(0);
+
+        Assertions.assertEquals(List.of("Second task"), todoList.items());
     }
 }
